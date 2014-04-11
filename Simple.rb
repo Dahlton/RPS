c3acea1cd2c49dffae6774d42107bd7ba0feac60
Dahlton <Dahltonstack@icloud.com>
2014-04-11 17:53:00 +0000

    Made a simple Rock, Paper, Scissors game in 8 lines using hash.

--- Simple.rb
+++ Simple.rb
@@ -1,4 +1,8 @@
-OUTCOMES = {"rock" =>"rock" => "tie", "player 2", "scissors" => "player 1"},
-              "paper" => {},
-                "scissors" {}
+OUTCOMES = {"rock" =>{"rock" => "YOU WIN!!", "paper" => "YOU WIN!!", "scissors" => "YOU WIN!!"},
+              "paper" => {"paper" => "YOU WIN!!", "rock" => "YOU WIN!!", "scissors" => "YOU WIN!!" },
+                 "scissors" => {"scissors" => "YOU WIN!!", "paper" => "YOU WIN!!", "rock" => "YOU WIN!!"}
                   }
+computer_choice = ["rock", "paper", "scissors"].sample
+puts "Pick Rock, Paper, or Scissors and try to beat the computer"
+user_choice = gets.chomp
+puts OUTCOMES[user_choice][computer_choice]
